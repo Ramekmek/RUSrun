55f6bb11f5faf6a408804cc984ce02db3bae3360
keemek <keemek.dadi@gmail.com>
2016-07-12 16:33:31 +0700

    finish 12July

--- app/src/main/java/rus/ramek/rusrun/SignUpActivity.java
+++ app/src/main/java/rus/ramek/rusrun/SignUpActivity.java
@@ -2,12 +2,42 @@ package rus.ramek.rusrun;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    //Explicit
+    private EditText nameEditText,userEditText, passwordEditText;
+    private RadioGroup radioGroup;
+    private RadioButton avatar0RadioButton,avatar1RadioButton,
+             avatar2RadioButton,avatar3RadioButton, avatar4RadioButton;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-    }
-}
+
+        //Bind Widget
+        nameEditText = (EditText) findViewById(R.id.editText);
+        userEditText = (EditText) findViewById(R.id.editText2);
+        passwordEditText = (EditText) findViewById(R.id.editText3);
+        radioGroup = (RadioGroup) findViewById(R.id.radAvatar);
+        avatar0RadioButton = (RadioButton) findViewById(R.id.radioButton);
+        avatar1RadioButton = (RadioButton) findViewById(R.id.radioButton2);
+        avatar2RadioButton = (RadioButton) findViewById(R.id.radioButton3);
+        avatar3RadioButton = (RadioButton) findViewById(R.id.radioButton4);
+        avatar4RadioButton = (RadioButton) findViewById(R.id.radioButton5);
+
+    }   //Main Method
+
+    public void clickSignUpSign(View view){
+
+    }   // clickSignUp
+
+
+}   //Main Class
